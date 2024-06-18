a9e2327059d9c3ddcf1e1d05e2245a97b732e1f4
Michael Studley <michael.r.studley@gmail.com>
2024-06-18 11:41:18 +0700

    Tenth push. Attempting to solve issues with private files

--- app/database/database.py
+++ app/database/database.py
@@ -3,12 +3,6 @@ import pymysql
 from pymysql.cursors import DictCursor
 # from app.config.config import Config
 
-MYSQL_HOST = os.environ.get('MYSQL_HOST')
-MYSQL_USER = os.environ.get('MYSQL_USER')
-MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD')
-MYSQL_DB = os.environ.get('MYSQL_DB')
-MYSQL_CURSOR_CLASS = os.environ.get('MYSQL_CURSOR_CLASS')
-
 
 class Database:
     conn: pymysql.Connection = None
@@ -17,24 +11,13 @@ class Database:
     @classmethod
     def init(cls):
         Database.conn = pymysql.connect(
-            host=MYSQL_HOST,
-            user=MYSQL_USER,
-            password=MYSQL_PASSWORD,
-            database=MYSQL_DB,
+            host=etc/secrets/Config.MYSQL_HOST,
+            user=etc/secrets/Config.MYSQL_USER,
+            password=etc/secrets/Config.MYSQL_PASSWORD,
+            database=etc/secrets/Config.MYSQL_DB,
             charset='utf8mb4',
-            cursorclass=MYSQL_CURSOR_CLASS)
+            cursorclass=etc/secrets/Config.MYSQL_CURSOR_CLASS)
         Database.curs = Database.conn.cursor()
-
-    # @classmethod
-    # def init(cls):
-    #     Database.conn = pymysql.connect(
-    #         host=Config.MYSQL_HOST,
-    #         user=Config.MYSQL_USER,
-    #         password=Config.MYSQL_PASSWORD,
-    #         database=Config.MYSQL_DB,
-    #         charset='utf8mb4',
-    #         cursorclass=Config.MYSQL_CURSOR_CLASS)
-    #     Database.curs = Database.conn.cursor()
 
     @classmethod
     def fetchall(cls, query: str, data: tuple = None) -> tuple:
